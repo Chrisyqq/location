c2592b6000c9fd637171b681059f33a776db99ca
Chris <chrisyqq1993@gmail.com>
2017-03-17 18:25:07 +0800

    no message

--- js/location-old2.js
+++ js/location-old2.js
@@ -11,24 +11,17 @@ var expectY; //点在图标内Y的位置
 var noData; //移动过程中 添加事件一直执行  创造此数据 为了移动的时候只执行一次
 var smallPlaceData; //图标狂位置信息状态传输
 var goBack = 1;
-var idLocation = 1;
-var idVidicon = 1;
 var pointData;
 
 
 $(document).on('mousedown',".iconfont",function (e){
     if(e.which==1){
         var nowThis = $(this);
-
         goBack = 1;
         expectX = e.pageX - nowThis.offset().left;
         expectY = e.pageY - nowThis.offset().top;
         noData = true;
 
-        //实时显示位置信息
-        $('#input').find('input').eq(0).attr('value',nowThis.css('left'));
-        $('#input').find('input').eq(1).attr('value',nowThis.css('top'));
-        $('#input').find('input').eq(2).attr('value',nowThis.attr('id'));
         //除了标签栏位置
         if((boxLeft<e.pageX  && e.pageY>boxTop+40 && noData==true) || (boxLeft+60 <e.pageX  && e.pageY>boxTop && noData==true)){
             noData = false;//
@@ -45,12 +38,12 @@ $(document).on('mousedown',".iconfont",function (e){
             nowThis.addClass('follow');
             var locationLeft = e.pageX - boxLeft - expectX;
             var locationTop = e.pageY - boxTop - expectY;
-
-            //case: 为移动出去又回到图标框 增加识别标签 goBack
             if((boxLeft<e.pageX  && e.pageY>boxTop+40) || (boxLeft+60 <e.pageX  && e.pageY>boxTop)){
                 goBack = goBack + 1;
             }else{
-
+                if(goBack>1){
+                    nowThis.remove();
+                }
             }
 
             if(-5 <= locationLeft && locationLeft <= (boxWidth - 20)){ //
@@ -63,10 +56,10 @@ $(document).on('mousedown',".iconfont",function (e){
                     if(locationTop > (boxHeight - 25)){
                         nowThis.css({"left":locationLeft,"top":(boxHeight - 25)});
                     }
-                    //
+
                     $(document).unbind("mousemove");
                     if(goBack>1){
-                        nowThis.remove();
+                        // nowThis.removeClass('follow');
                     }else{
                         nowThis.removeClass('follow');
                     }
@@ -80,49 +73,63 @@ $(document).on('mousedown',".iconfont",function (e){
                 }
                 $(document).unbind("mousemove");
                 if(goBack>1){
-                    nowThis.remove();
+
                 }else{
                     nowThis.removeClass('follow');
                 }
             }
 
+            //case: 为移动出去又回到图标框 增加识别标签 goBack
+
+
             //移动出40*90的小框后自动生成 图标
             if((boxLeft<e.pageX  && e.pageY>boxTop+40 && noData==true) || (boxLeft+60 <e.pageX  && e.pageY>boxTop && noData==true)){
-                if(nowThis.hasClass('icon-location')){
-                    idLocation = idLocation+1;
-                    $('#locationBarPlace').append('<i id="'+ "location" + idLocation +'" class="iconfont icon-location"></i>');
-                    noData = false;
-                }else{
-                    idVidicon = idVidicon+1;
-                    $('#locationBarPlace').prepend('<i id="'+ "vidicon" + idVidicon +'" class="iconfont icon-icon-technology-vidicon"></i>');
-                    noData = false;
-                }
+                // if(goBack=1){
+                //     if(nowThis.hasClass('icon-location')){
+                //         console.log(nowThis)
+                //         $('#locationBarPlace').append('<i class="iconfont icon-location"></i>');
+                //         noData = false;
+                //     }else{
+                //         console.log(nowThis)
+                //         $('#locationBarPlace').prepend('<i id="technologyVidico" class="iconfont icon-icon-technology-vidicon"></i>');
+                //         noData = false;
+                //     }
+                //     console.log(goBack)
+                // }
             }else{
 
             }
 
-            nowThis.attr('x',nowThis.css('left'));
-            nowThis.attr('y',nowThis.css('left'));
-            $('#input').find('input').eq(2).attr('value',nowThis.attr('id'));
+            $('#input').text(e.pageX+","+e.pageY+","+boxWidth+",");
 
-            //实时显示位置信息
-            $('#input').find('input').eq(0).attr('value',nowThis.css('left'));
-            $('#input').find('input').eq(1).attr('value',nowThis.css('top'));
-            $('#input').find('input').eq(2).attr('value',nowThis.attr('id'));
+
+            //移动结束清除移动 鼠标起来
 
         });
         //移动结束清除移动
         nowThis.mouseup(function (e) {
-            if((boxLeft<e.pageX  && e.pageY>boxTop+40) || (boxLeft+60 <e.pageX  && e.pageY>boxTop)){
 
+            if((boxLeft<e.pageX  && e.pageY>boxTop+40) || (boxLeft+60 <e.pageX  && e.pageY>boxTop)){
+                if(goBack=1){
+                    if(nowThis.hasClass('icon-location')){
+                        console.log(nowThis);
+                        $('#locationBarPlace').append('<i class="iconfont icon-location"></i>');
+                        noData = false;
+                    }else{
+                        console.log(nowThis);
+                        $('#locationBarPlace').prepend('<i id="technologyVidico" class="iconfont icon-icon-technology-vidicon"></i>');
+                        noData = false;
+                    }
+                    console.log(goBack)
+                }
             }else{
                 if (smallPlaceData==false){
                     //删除点位置
-                    nowThis.remove();
+                    // nowThis.remove();
                 }else{
                     if(goBack > 1){
-                        //删除点位置 移动出之后再回来
-                        nowThis.remove();
+                        // nowThis.remove();
+
                     }else{
                         // nowThis.remove();
                         nowThis.removeClass('follow')
@@ -131,22 +138,40 @@ $(document).on('mousedown',".iconfont",function (e){
                 }
 
             }
+            // return moveState;
+            //移动出40*90的小框后自动生成 图标
             $(document).unbind("mousemove");
             goBack = 1;
+            console.log(nowThis)
         });
+        function likeMouseUp(e) {
+            if((boxLeft<e.pageX  && e.pageY>boxTop+40) || (boxLeft+60 <e.pageX  && e.pageY>boxTop)){
+
+            }else{
+                if (smallPlaceData==false){
+                    //删除点位置
+                    nowThis.remove();
+                }else{
+                    if(goBack > 1){
+                        nowThis.remove();
+
+                    }else{
+                        // nowThis.remove();
+                        // nowThis.removeClass('follow')
+                    }
+
+                }
+
+            }
+            // return moveState;
+            //移动出40*90的小框后自动生成 图标
+            $(document).unbind("mousemove");
+            goBack = 1;
+        }
     }
 
 });
-//鼠标右键屏蔽
-$('#locationPlace').on('contextmenu',function(e){
-    e.preventDefault();
-});
-$(document).on('mousedown',".follow",function (e){
-    if(e.which==3){
-        $('.follow-message').html('<p>'+$(this).attr('x')+'</p><p>'+$(this).attr('y')+'</p><p>'+$(this).attr('id')+'</p>');
-        $('.follow-message').css({'left': $(this).css('left'),'top': $(this).css('top')});
-        e.preventDefault();
-    }
-});
+
+
 
 
